ab195b3c4f876124671171ccea778da944a4d06d
Elo Okonkwo <elo.okonkwo@gmail.com>
2017-09-14 17:46:46 +0100

    setting uo the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -5,12 +5,19 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('combined'))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+// app.get('/status', (req, res) => {
+//   res.send({
+//     message: 'hello world!'
+//   })
+// })
+
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Your user with email ${req.body.email} was registerd!`
   })
 })
 
