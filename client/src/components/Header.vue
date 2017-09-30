52d56cdbe15931fca01b99a349d9f51f2f497d29
Elo Okonkwo <elo.okonkwo@gmail.com>
2017-09-30 07:49:01 +0100

    creating user authentication

---
+++ client/src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-title class="mr-4">
+      <span
+      class='home'
+        @click="navigateTo({ name: 'root' })">
+      Tabtracker
+      </span>
+    </v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat dark>
+        Browse
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat dark
+        @click="navigateTo({ name: 'Login' })">
+        Login
+      </v-btn>
+      <v-btn flat dark
+        @click="navigateTo({ name: 'Register' })">
+        Sign Up
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  cursor: pointer;
+}
+</style>
---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,35 @@
+const Joi = require('joi')
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+      )
+    }
+    const {error, value} = Joi.validate(req.body, schema)
+    console.log(value, 'this value')
+    console.log(error, 'this error')
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a valid email address'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: 'The password provided doesnt match the right format'
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
